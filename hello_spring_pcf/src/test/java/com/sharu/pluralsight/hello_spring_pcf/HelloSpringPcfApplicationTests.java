3f92ba109b5eb7f63d3575d685a84fd44b01c04e
saravanan75 <43355335+saravanan75@users.noreply.github.com>
2020-10-22 12:29:35 -0500

    Hello Spring pcf - Initial commit

---
+++ hello_spring_pcf/src/test/java/com/sharu/pluralsight/hello_spring_pcf/HelloSpringPcfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharu.pluralsight.hello_spring_pcf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringPcfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
